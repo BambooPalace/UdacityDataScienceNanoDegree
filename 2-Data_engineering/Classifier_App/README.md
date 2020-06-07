1423445049c18e6ffa4f927b8ffef6822dd3c030
Claire Gong <aprilgng@gmail.com>
2020-06-07 23:51:39 +0800

    web app ready to sail, complete README

---
+++ 2-Data_engineering/Classifier_App/README.md
@@ -0,0 +1,67 @@
+
+### Table of Contents
+
+1. [Installation](#installation)
+2. [Project Motivation](#motivation)
+3. [File Descriptions](#files)
+4. [Instructions](#instructions)
+4. [Licensing, Authors, and Acknowledgements](#licensing)
+
+## Installation <a name="installation"></a>
+
+The code run with Python3 and some additional packages beside Anaconda pre-installations, i.e. wordcloud and plotly. The machine learning model for NLP is ComplementNB, which is available for scikit-learn 0.20+.
+
+## Project Motivation<a name="motivation"></a>
+
+This project takes in 25000+ disaster messagess to build a classifier for categorizing new messages. 
+The project is done in three steps:
+1. ETL pipeline: extract raw data, transform/clean data, load wrangled data to database.
+2. NLP ML pipeline: read and process text data from database, train and save the machine learning model.
+3. Flask Web App: load trained model to classify new messages and display analysis data on webpages, using Flask framework.
+
+
+## File Descriptions <a name="files"></a>
+
+App data is in [Classifier_App](http:/) folder, project code structure is as below: 
+- app
+| - template
+| |- master.html  # main page of web app
+| |- go.html  # classification result page of web app
+|- run.py  # Flask file that runs app
+
+- data
+|- disaster_categories.csv  # data to process 
+|- disaster_messages.csv  # data to process
+|- process_data.py
+|- InsertDatabaseName.db   # database to save clean data to
+
+- models
+|- train_classifier.py
+|- classifier.pkl  # saved model         
+
+Other folders contain preparation data and knowledge files.
+
+## Intructions to run app<a name="instructions"></a>
+1. Run the following commands in the project's root directory to set up your database and model.
+- update scikit-learn to 0.20+ 
+
+    - To run ETL pipeline that cleans data and stores in database, in command line:
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+
+    - To run ML pipeline that trains classifier and saves, in command line:
+        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
+
+
+2. Run the following command in the app's directory to run your web app.
+- install wordcloud and plotly before run
+- in command line:
+    `python app/run.py`
+ 
+
+3. Go to https://SPACEID-3001.SPACEDOMAIN to retrieve app.
+
+
+## Licensing, Authors, Acknowledgements<a name="licensing"></a>
+
+For the sharing spirit of internet, you are free to use it following License [CC0: Public Domain](https://creativecommons.org/publicdomain/zero/1.0/).
+
---
+++ 2-Data_engineering/Classifier_App/models/test_train_classifier.py
@@ -0,0 +1,104 @@
+import warnings
+warnings.filterwarnings("ignore")# for warning when model handle sparse matrix
+                        
+import sys
+# import libraries
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from sqlalchemy import create_engine
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import f1_score
+from joblib import dump,load
+
+nltk.download('punkt') #tokenizer
+nltk.download('stopwords') 
+nltk.download('wordnet')# lemmatizer
+
+def load_data(database_filepath):
+    #load data from database
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql('SELECT * FROM messages', engine)
+    X = df.message.values
+    Y = df.iloc[:,2:].values
+    labels=df.columns[2:].tolist()
+    return X,Y,labels
+
+
+def tokenize(text):
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    # tokenize text
+    tokens = word_tokenize(text)
+    # lemmatize and remove stop words
+    lemmatizer = WordNetLemmatizer()
+    stop_words = stopwords.words("english")
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+    return tokens
+
+
+def build_model():
+    #build a pipeline including feature extraction and modelling (Naive Baiyes)
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(BernoulliNB()))
+     ])
+    #set gridsearch parameters 
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.75, 1.0),
+        'tfidf__use_idf': (True, False)
+    }
+    cv = GridSearchCV(pipeline, param_grid=parameters, cv=3)
+    return cv
+
+
+def evaluate_model(model, X_test, Y_test,category_names):
+    Y_preds=model.predict(X_test)
+    n=len(category_names)
+    f1_list=[f1_score(Y_test[:,i], Y_preds[:,i],average='weighted')for i in range(n)]
+    print('model mean f1_score on test set is: ', sum(f1_list)/n)
+
+
+def save_model(model, model_filepath):
+    dump(model, model_filepath)
+
+# below are my functions for testing
+def load_model():
+    # test show error if use sklearn.externals.joblib to unload
+    model = load('models/classifier.pkl') 
+    return model
+
+def display_category(text,model):
+    # display prediction of a message in dataframe format
+    cat=model.predict(text)[0]
+    df=pd.DataFrame({'prediction': cat}, index=labels)
+    df= df[df.prediction!=0]
+    print(df)
+
+def test_classifier(text):
+    #load model and run on a test text sample
+
+    model=load_model()
+    df=display_category(text,model)
+    print(df)
+
+
+# python models/test_train_classifier.py data/DisasterResponse.db models/classifier.pkl
+if __name__ == '__main__':
+    _,_,labels=load_data('data/DisasterResponse.db')
+    text=["I hear thunder"]        
+    print('Classifier for below message: \n\t'+ text[0])
+    test_classifier(text)
+
+  
+    
